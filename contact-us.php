bee2ec2209ffec54a310e7f2e5f1703df6fb81ba
Atharva-Kinhikar <katharva10@gmail.com>
2024-07-20 10:12:32 +0530

    Update contact-us.php

--- contact-us.php
+++ contact-us.php
@@ -11,33 +11,19 @@
                </tr>
                
                <tr>
-                    <td align="left">Yash Paratane</td>
-                    <td><a href="mailto:yash.paratane@iccs.ac.in"> yash.paratane@iccs.ac.in </a></td>
+                    <td align="left">Atharva</td>
+                    <td><a href="mailto:Atharva.kinhikar@iccs.ac.in"> Atharva.kinhikar@iccs.ac.in </a></td>
                </tr>
-               <tr>
-                    <td align="left">Aakanksha Jodh</td>
-                    <td><a href="mailto:aakanksha.jodh@iccs.ac.in"> aakanksha.jodh@iccs.ac.in </a></td>
-               </tr>
-               <tr>
-                    <td align="left">Shailesh Mukund Mestry</td>
-                    <td><a href="mailto:shailesh.mestry@iccs.ac.in"> shailesh.mestry@iccs.ac.in </a></td>
-               </tr>
+          
                <tr>
                     <th colspan="2" bgcolor="#ff00ff">Mobile Number </th>
                </tr>
                
                <tr>
-                    <td> Yash Paratane</td>
-                    <td><a href="tel:9028718437"> +91 90287 18437</a></td>
+                    <td> Atharva Kinhikar</td>
+                    <td><a href="tel:8830902361"> +91 8830902361</a></td>
                </tr>
-               <tr>
-                    <td> Aakanksha Jodh</td>
-                    <td><a href="tel:9423429293"> +91 94234 29293</a></td>
-               </tr>
-               <tr>
-                    <td> Shailesh Mukund Mestry</td>
-                    <td><a href="tel:7972382320"> +91 79723 82320 </a></td>
-               </tr>
+            
           </table>
           </center>
           <br><br>
